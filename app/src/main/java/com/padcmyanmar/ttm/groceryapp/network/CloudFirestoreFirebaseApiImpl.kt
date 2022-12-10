74d7235fe312e35417b24631875597416ac94b29
Thu Thu <ms.jellyfishgirl@gmail.com>
2022-12-10 07:11:52 +0630

    Cloud firestore add data

--- app/src/main/java/com/padcmyanmar/ttm/groceryapp/network/CloudFirestoreFirebaseApiImpl.kt
+++ app/src/main/java/com/padcmyanmar/ttm/groceryapp/network/CloudFirestoreFirebaseApiImpl.kt
@@ -1,5 +1,6 @@
 package com.padcmyanmar.ttm.groceryapp.network
 
+import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.padcmyanmar.ttm.groceryapp.data.vos.GroceryVO
@@ -57,7 +58,21 @@ object CloudFirestoreFirebaseApiImpl : FirebaseApi{
     }
 
     override fun addGrocery(name: String, description: String, amount: Int) {
-        TODO("Not yet implemented")
+        val groceryMap : HashMap<String,Any> = hashMapOf(
+            "name" to name,
+            "description" to description,
+            "amount" to amount.toLong()
+        )
+
+        db.collection("groceries")
+            .document(name)
+            .set(groceryMap)
+            .addOnSuccessListener {
+                Log.d("Success","Successfully added grocery")
+            }
+            .addOnFailureListener {
+                Log.d("Failure","Failed to add grocery")
+            }
     }
 
     override fun deleteGrocery(name: String) {
